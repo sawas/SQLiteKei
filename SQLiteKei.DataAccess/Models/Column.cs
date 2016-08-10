d67e260198f5c0fd6c25f2c5d033ef2838a4af4d
ShaezoNai <matthias.rueckbrodt@gmail.com>
2016-08-10 17:02:48 +0200

    Add some xml comments and update project version

--- SQLiteKei.DataAccess/Models/Column.cs
+++ SQLiteKei.DataAccess/Models/Column.cs
@@ -5,16 +5,52 @@
     /// </summary>
     public class Column
     {
+        /// <summary>
+        /// Gets or sets the Id of the column.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the column's name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the type of the column.
+        /// </summary>
+        /// <value>
+        /// The type of the data.
+        /// </value>
         public string DataType { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the column is defined as NOT NULL.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this column is not nullable; otherwise, <c>false</c>.
+        /// </value>
         public bool IsNotNullable { get; set; }
 
-        public object DefaultValue { get; set; }
-
+        /// <summary>
+        /// Gets or sets a value indicating whether the column is defined as PRIMARY.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this column is primary; otherwise, <c>false</c>.
+        /// </value>
         public bool IsPrimary { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default value of the column.
+        /// </summary>
+        /// <value>
+        /// The default value.
+        /// </value>
+        public object DefaultValue { get; set; }
     }
 }
--- SQLiteKei.DataAccess/Models/Table.cs
+++ SQLiteKei.DataAccess/Models/Table.cs
@@ -5,10 +5,28 @@
     /// </summary>
     public class Table
     {
+        /// <summary>
+        /// Gets or sets the name of the on which the table was created.
+        /// </summary>
+        /// <value>
+        /// The name of the database.
+        /// </value>
         public string DatabaseName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the table..
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the SQL statement that was used to create the table.
+        /// </summary>
+        /// <value>
+        /// The SQL statement.
+        /// </value>
         public string CreateStatement { get; set; }
     }
 }
