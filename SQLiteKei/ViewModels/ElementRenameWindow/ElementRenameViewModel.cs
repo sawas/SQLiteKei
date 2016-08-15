67c1c2cea75571f496fad884e9a07e88cf0a5da1
ShaezoNai <matthias.rueckbrodt@gmail.com>
2016-08-15 12:14:38 +0200

    Continue work on rename window

--- SQLiteKei/ViewModels/ElementRenameWindow/ElementRenameViewModel.cs
+++ SQLiteKei/ViewModels/ElementRenameWindow/ElementRenameViewModel.cs
@@ -1,11 +1,19 @@
-﻿using SQLiteKei.Commands;
+﻿using log4net;
+
+using SQLiteKei.Commands;
 using SQLiteKei.ViewModels.Base;
+using SQLiteKei.ViewModels.MainWindow.DBTreeView;
 using SQLiteKei.ViewModels.MainWindow.DBTreeView.Base;
+using SQLiteKei.Util;
+
+using System;
 
 namespace SQLiteKei.ViewModels.ElementRenameWindow
 {
     public class ElementRenameViewModel : NotifyingModel
     {
+        private readonly ILog logger = LogHelper.GetLogger();
+
         private TreeItem originalElement;
 
         public string NewName { get; set; }
@@ -26,6 +34,43 @@ namespace SQLiteKei.ViewModels.ElementRenameWindow
         private void Rename()
         {
             StatusInfo = string.Empty;
+            var itemType = originalElement.GetType();
+
+            if (itemType == typeof(DatabaseItem))
+                RenameDatabase();
+            else if (itemType == typeof(TableItem))
+                RenameTable();
+            else if (itemType == typeof(ViewItem))
+                RenameView();
+            else if (itemType == typeof(IndexItem))
+                RenameIndex();
+            else if (itemType == typeof(TriggerItem))
+                RenameTrigger();
+        }
+
+        private void RenameDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RenameTable()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RenameView()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RenameIndex()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RenameTrigger()
+        {
+            throw new NotImplementedException();
         }
 
         private DelegateCommand renameCommand;
